0e24dc83b20a3c2a89b50c263c5b6909afa5c386
James W. Lane <james.w.lane@mac.com>
2017-11-09 14:53:09 -0600

    build(Updating build tools): Adding in build tools for future use

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "globals": {
+    "ts-jest": {
+      "tsConfigFile": "tsconfig.json"
+    }
+  },
+  "transform": {
+    "^.+\\.(ts|tsx)$": "./node_modules/ts-jest/preprocessor.js"
+  },
+  "testMatch": [
+    "**/tests/**/*.test.(ts|tsx|js)"
+  ],
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js"
+  ],
+  "clearMocks": true
+};

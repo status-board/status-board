712baa8da5359c12fe848e32d18dbd0b33a45b2f
James W. Lane <james.w.lane@mac.com>
2017-12-13 17:09:47 -0600

    fix(Job Dependencies): Fixed loader error handler for job dependencies

--- src/job-dependencies/loader.ts
+++ src/job-dependencies/loader.ts
@@ -14,7 +14,11 @@ export function fillDependencies(jobWorker: any, deps: any) {
         const depPath = path.join(folderPath, 'dependency.js');
         jobWorker.dependencies[dependencyFolders[i]] = require(depPath)(jobWorker, deps);
       } catch (error) {
-        throw Error(`error resolving dependency ${dependencyFolders[i]}. ERROR:${error}`);
+        throw Error(`
+          Error resolving dependency: ${dependencyFolders[i]}
+          Dependencies required: ${path.join(folderPath, 'dependency.js')}
+          Stack trace: ${error}
+        `);
       }
     }
   }

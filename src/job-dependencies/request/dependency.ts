1cf3fe5312314b5e9f79279a12fb64c8498c86ba
James W. Lane <james.w.lane@mac.com>
2017-12-13 19:01:06 -0600

    fix(Job Dependencies): Fixed no such file or directory, open '../../../package.json' in request

--- src/job-dependencies/request/dependency.ts
+++ src/job-dependencies/request/dependency.ts
@@ -1,7 +1,8 @@
 import * as fs from 'fs';
+import * as path from 'path';
 import * as request from 'request';
 
-const projectPackageJson = fs.readFileSync('../../../package.json');
+const projectPackageJson = fs.readFileSync(path.join(__dirname, '../../..', 'package.json'));
 
 export default function () {
   return request.defaults({

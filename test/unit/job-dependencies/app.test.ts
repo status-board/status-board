32ec401ca7491f3b446019fec40ed2b06c8ceb96
James W. Lane <james.w.lane@mac.com>
2017-12-27 11:02:10 -0600

    test(job-dependencies): Updated testing for app job dependencies

--- test/unit/job-dependencies/app.test.ts
+++ test/unit/job-dependencies/app.test.ts
@@ -1,7 +1,18 @@
-import {} from '../../../src/job-dependencies/app/dependency';
+import * as Chance from 'chance';
+import dependency from '../../../src/job-dependencies/app/dependency';
 
-describe('Test', () => {
-  test('Test', () => {
-    expect('foo').toMatch('foo');
+const chance = new Chance();
+
+describe('Job Dependencies: App', () => {
+  test('Should return the dependencies app', () => {
+    const jobWorker: any = {};
+    jobWorker[chance.string()] = chance.string();
+    const deps = {
+      app: chance.string(),
+    };
+
+    const response = dependency(jobWorker, deps);
+
+    expect(response).toEqual(deps.app);
   });
 });

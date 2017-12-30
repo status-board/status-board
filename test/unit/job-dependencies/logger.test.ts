014a280aac87de3c1f24ba26dad968a43f1addc6
James W. Lane <james.w.lane@mac.com>
2017-12-30 10:29:17 -0600

    test(job-dependencies): Added test for logger job dependencie

--- test/unit/job-dependencies/logger.test.ts
+++ test/unit/job-dependencies/logger.test.ts
@@ -1,7 +1,20 @@
-import {} from '../../../src/job-dependencies/logger/dependency';
+import dependency from '../../../src/job-dependencies/logger/dependency';
+import logger from '../../../src/logger';
 
-describe('Test', () => {
-  test('Test', () => {
-    expect('foo').toMatch('foo');
+const jobWorker = jest.fn();
+const deps = {
+  io: jest.fn(),
+};
+
+jest.mock('../../../src/logger', () => {
+  return { default: jest.fn() };
+});
+
+describe('Job Dependencies: Logger', () => {
+  test('Logger should be returned by dependency', () => {
+    dependency(jobWorker, deps);
+
+    expect(logger).toBeCalled();
+    expect(logger).toBeCalledWith(jobWorker, deps.io);
   });
 });

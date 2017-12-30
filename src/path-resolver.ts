1f7a2308584577abb06dde5227b9f0a44a4cea2a
Swami Kalagiri <swami.kalagiri@gmail.com>
2017-12-30 07:07:47 -0600

    test(path-resolver): Adding tests

--- src/path-resolver.ts
+++ src/path-resolver.ts
@@ -4,13 +4,13 @@ function getFromRootPath(paths: string[]) {
   return path.join(...paths);
 }
 
-export function fromAtlasboard(...args: any[]) {
+export function fromAtlasboard(...args: string[]) {
   const paths = [__dirname];
   paths.push(...args);
   return getFromRootPath(paths);
 }
 
-export function fromLocalWallboard(...args: any[]) {
+export function fromLocalWallboard(...args: string[]) {
   const paths = [process.cwd()];
   paths.push(...args);
   return getFromRootPath(paths);
--- test/unit/path-resolver.test.ts
+++ test/unit/path-resolver.test.ts
@@ -1,7 +1,20 @@
-import {} from '../../src/path-resolver';
+import * as path from 'path';
+import {fromAtlasboard, fromLocalWallboard} from '../../src/path-resolver';
 
-describe('Test', () => {
-  test('Test', () => {
-    expect('foo').toMatch('foo');
+const path1: string = '/path1';
+const path2: string = '/path2';
+
+describe('Path Resolver', () => {
+  test('Should resolve the paths from local wall board', () => {
+    const paths: string[] = [process.cwd()];
+    paths.push(path1, path2);
+
+    expect(fromLocalWallboard(path1, path2))
+      .toMatch(path.join(...paths));
+  });
+
+  test('Should resolve the paths from atlas board', () => {
+    expect(fromAtlasboard(path1, path2))
+      .toMatch('\\src\\path1\\path2');
   });
 });

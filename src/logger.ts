b8bef6eddfc21c562e2a800a0daf2947994d8ff4
James W. Lane <james.w.lane@mac.com>
2018-01-05 15:36:32 -0600

    fix(logger): Commenting out code thats breaking Status Board from starting
    Creating a issue https://github.com/jameswlane/status-board/issues/57

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,7 @@
 import { Console } from 'console';
 import configManager from './config-manager';
 
-const config = configManager('logging');
+// const config = configManager('logging');
 export const logger = new Console(process.stdout, process.stderr);
 export function formatConsoleDate(date: Date) {
   const hour = date.getHours();
@@ -22,18 +22,18 @@ export function formatConsoleDate(date: Date) {
 
 export default function (jobWorker?: any, io?: SocketIO.Server) {
   // jobWorker and socket.io instance are optional
-  const loggerConfig = config.logger || {};
+  // const loggerConfig = config.logger || {};
   // tslint:disable-next-line max-line-length
-  const prefix = jobWorker ? `[dashboard: ${jobWorker.dashboard_name}] [job: ${jobWorker.job_name}] ` : '';
+  // const prefix = jobWorker ? `[dashboard: ${jobWorker.dashboard_name}] [job: ${jobWorker.job_name}] ` : '';
 
-  loggerConfig.transport = (data: any) => {
-    const logText = prefix + data.output;
-    // tslint:disable-next-line no-console
-    console.log(logText);
-    if (io) {
-      io.emit('server', { type: data.level, msg: logText });
-    }
-  };
+  // loggerConfig.transport = (data: any) => {
+  //   const logText = prefix + data.output;
+  //   // tslint:disable-next-line no-console
+  //   console.log(logText);
+  //   if (io) {
+  //     io.emit('server', { type: data.level, msg: logText });
+  //   }
+  // };
 
   return logger;
 }

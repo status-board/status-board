e380c29d6499af0e9f5f5a75c6675eb5b6c1688c
James W. Lane <james.w.lane@mac.com>
2018-03-13 18:48:01 -0500

    test(widget): Improved test for load-css-if-present.ts

--- test/unit/webapp/routes/widget/load-css-if-present.test.ts
+++ test/unit/webapp/routes/widget/load-css-if-present.test.ts
@@ -1,29 +1,92 @@
 import * as Chance from 'chance';
-import { Request } from 'jest-express/lib/request';
 import { Response } from 'jest-express/lib/response';
 import { system } from '../../../../helpers/chance-system';
-
-import { log } from '../../../../../src/webapp/routes/widget';
+import { noop } from '../../../../../src/helpers';
+import { loadCSSIfPresent } from '../../../../../src/webapp/routes/widget';
+import * as addNamespacesCSSToResponse from '../../../../../src/webapp/routes/widget/add-namespaces-css-to-response';
+import * as getFileContents from '../../../../../src/webapp/routes/widget/get-file-contents';
 
 const chance = new Chance();
 chance.mixin(system);
 
 describe('Webapp: Widget: Load CSS If Present', () => {
-  let request: any;
-  let response: any;
+  let response: Response;
+  let code: string;
 
   beforeEach(() => {
-    request = new Request();
     response = new Response();
+    code = `div{ background-color:${chance.color({ format: 'hex' })} }`;
+    jest.spyOn(getFileContents, 'getFileContents')
+      .mockImplementation((ext, name, path, cb) => {
+        if (name === 'RETURN_NO_CSS') {
+          cb(null, null);
+        } else if (name === 'RETURN_ERROR') {
+          cb('GET_FILE_CONTENTS_ERROR');
+        } else {
+          cb(null, code);
+        }
+      });
+    jest.spyOn(addNamespacesCSSToResponse, 'addNamespacesCSSToResponse')
+      .mockImplementation(noop);
   });
 
   afterEach(() => {
-    request.resetMocked();
     response.resetMocked();
   });
 
-  test('test', () => {
-    // widget.addNamespace();
-    expect(true).toEqual(true);
+  test('should get the files content and add it to the namespace', () => {
+    const widgetName = chance.name();
+    const packagesPath = chance.system.filePath();
+    loadCSSIfPresent(response, widgetName, packagesPath, (error) => {
+      expect(error).toBeNull();
+      expect(getFileContents.getFileContents)
+        .toHaveBeenCalledWith(
+          '.css',
+          widgetName,
+          packagesPath,
+          expect.any(Function),
+        );
+      expect(addNamespacesCSSToResponse.addNamespacesCSSToResponse)
+        .toHaveBeenCalledWith(
+          code,
+          widgetName,
+          response,
+        );
+    });
+  });
+
+  test('should return nothing if there is no css', () => {
+    const widgetName = 'RETURN_NO_CSS';
+    const packagesPath = chance.system.filePath();
+    loadCSSIfPresent(response, widgetName, packagesPath, (error) => {
+      expect(error).toBeNull();
+      expect(getFileContents.getFileContents)
+        .toHaveBeenCalledWith(
+          '.css',
+          widgetName,
+          packagesPath,
+          expect.any(Function),
+        );
+      expect(addNamespacesCSSToResponse.addNamespacesCSSToResponse)
+        .not.toHaveBeenCalled();
+    });
+  });
+
+  test('should return error from get file content', () => {
+    const widgetName = 'RETURN_ERROR';
+    const packagesPath = chance.system.filePath();
+    loadCSSIfPresent(response, widgetName, packagesPath, (error) => {
+      expect(error).toEqual('GET_FILE_CONTENTS_ERROR');
+      expect(getFileContents.getFileContents)
+        .toHaveBeenCalledWith(
+          '.css',
+          widgetName,
+          packagesPath,
+          expect.any(Function),
+        );
+      expect(addNamespacesCSSToResponse.addNamespacesCSSToResponse)
+        .not.toHaveBeenCalled();
+
+    });
   });
 });

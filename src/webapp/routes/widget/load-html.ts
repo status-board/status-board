1fe3703ca4f66f930bbe095371dea72bb18d4def
James W. Lane <james.w.lane@mac.com>
2018-03-13 19:19:31 -0500

    test(widget): Improved test for load-html.ts

--- src/webapp/routes/widget/load-html.ts
+++ src/webapp/routes/widget/load-html.ts
@@ -1,7 +1,7 @@
 import { Response } from 'express';
 import { getFileContents } from './get-file-contents';
 
-export function loadHTML(response: Response, widgetName: any, packagesPath: any, cb: any) {
+export function loadHTML(response: Response, widgetName: string, packagesPath: string, cb: any) {
   getFileContents('.html', widgetName, packagesPath, (error: any, html: any) => {
     if (!error && html) {
       response.write(html);
--- test/unit/webapp/routes/widget/load-html.test.ts
+++ test/unit/webapp/routes/widget/load-html.test.ts
@@ -1,29 +1,80 @@
 import * as Chance from 'chance';
-import { Request } from 'jest-express/lib/request';
 import { Response } from 'jest-express/lib/response';
 import { system } from '../../../../helpers/chance-system';
 
-import { log } from '../../../../../src/webapp/routes/widget';
+import { loadHTML } from '../../../../../src/webapp/routes/widget';
+import * as getFileContents from '../../../../../src/webapp/routes/widget/get-file-contents';
 
 const chance = new Chance();
 chance.mixin(system);
 
 describe('Webapp: Widget: Load HTML', () => {
-  let request: any;
-  let response: any;
+  let response: Response;
+  let code: string;
 
   beforeEach(() => {
-    request = new Request();
     response = new Response();
+    response.write = jest.fn();
+    code = '<!doctype html><html><head></head><body></body></html>';
+    jest.spyOn(getFileContents, 'getFileContents')
+      .mockImplementation((ext, name, path, cb) => {
+        if (name === 'RETURN_NO_HTML') {
+          cb(null, null);
+        } else if (name === 'RETURN_ERROR') {
+          cb('GET_FILE_CONTENTS_ERROR');
+        } else {
+          cb(null, code);
+        }
+      });
   });
 
   afterEach(() => {
-    request.resetMocked();
     response.resetMocked();
+    jest.restoreAllMocks();
   });
 
-  test('test', () => {
-    // widget.addNamespace();
-    expect(true).toEqual(true);
+  test('should get the files content and add wite response', () => {
+    const widgetName = chance.name();
+    const packagesPath = chance.system.filePath();
+    loadHTML(response, widgetName, packagesPath, (error) => {
+      expect(error).toBeNull();
+      expect(getFileContents.getFileContents).toHaveBeenCalledWith(
+        '.html',
+        widgetName,
+        packagesPath,
+        expect.any(Function),
+      );
+      expect(response.write).toHaveBeenCalledWith(code);
+    });
+  });
+
+  test('should return nothing if there is no html and not write a response', () => {
+    const widgetName = 'RETURN_NO_HTML';
+    const packagesPath = chance.system.filePath();
+    loadHTML(response, widgetName, packagesPath, (error) => {
+      expect(error).toBeNull();
+      expect(getFileContents.getFileContents).toHaveBeenCalledWith(
+        '.html',
+        widgetName,
+        packagesPath,
+        expect.any(Function),
+      );
+      expect(response.write).not.toHaveBeenCalled();
+    });
+  });
+
+  test('should return error from get file content and not write a response', () => {
+    const widgetName = 'RETURN_ERROR';
+    const packagesPath = chance.system.filePath();
+    loadHTML(response, widgetName, packagesPath, (error) => {
+      expect(error).toEqual('GET_FILE_CONTENTS_ERROR');
+      expect(getFileContents.getFileContents).toHaveBeenCalledWith(
+        '.html',
+        widgetName,
+        packagesPath,
+        expect.any(Function),
+      );
+      expect(response.write).not.toHaveBeenCalled();
+    });
   });
 });

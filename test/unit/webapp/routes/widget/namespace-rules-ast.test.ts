a33bd1b4b9abdbd251f0648b46d21a195a896df8
James W. Lane <james.w.lane@mac.com>
2018-03-14 12:13:55 -0500

    test(Improved testing for namespace-rules-ast.ts):

--- test/unit/webapp/routes/widget/namespace-rules-ast.test.ts
+++ test/unit/webapp/routes/widget/namespace-rules-ast.test.ts
@@ -1,9 +1,9 @@
 import * as Chance from 'chance';
+import * as css from 'css';
 import { Request } from 'jest-express/lib/request';
 import { Response } from 'jest-express/lib/response';
+import { namespaceRulesAST } from '../../../../../src/webapp/routes/widget';
 import { system } from '../../../../helpers/chance-system';
-
-import { log } from '../../../../../src/webapp/routes/widget';
 
 const chance = new Chance();
 chance.mixin(system);
@@ -22,8 +22,50 @@ describe('Webapp: Widget: Namespace Rules AST', () => {
     response.resetMocked();
   });
 
-  test('test', () => {
-    // widget.addNamespace();
-    expect(true).toEqual(true);
+  test('should namespace selectors', () => {
+    const code = `div{ background-color:${chance.color({ format: 'hex' })} }`;
+    const namespace = chance.string({ pool: 'abcdefghijklmnopqrstuvwxyz' });
+    const stylesheet = css.parse(code);
+    const expectedSelector = `#widgets-container > ul > li[data-widget-id="${namespace}"] div`;
+    const rules = stylesheet.stylesheet.rules[0];
+
+    const selector = rules.selectors[0];
+    expect(selector).toMatch('div');
+
+    namespaceRulesAST(stylesheet.stylesheet, namespace);
+
+    const namespacedSelector = rules.selectors[0];
+    expect(namespacedSelector).toMatch(expectedSelector);
+  });
+
+  test('should not namespace fonts', () => {
+    const fontFamily = chance.name();
+    const fontFile = chance.system.fileName();
+    const code = `@font-face{font-family:'${fontFamily}';src:url('${fontFile}');}`;
+    const namespace = chance.string({ pool: 'abcdefghijklmnopqrstuvwxyz' });
+    const stylesheet = css.parse(code);
+    const rules = stylesheet.stylesheet.rules[0];
+
+    expect(rules).not.toHaveProperty('selectors');
+
+    namespaceRulesAST(stylesheet.stylesheet, namespace);
+
+    expect(rules).not.toHaveProperty('selectors');
+  });
+
+  test('should namespace selectors in media queries', () => {
+    const code = `@media screen and (min-width: 1200px) {div{ background-color:${chance.color({ format: 'hex' })} }}`;
+    const namespace = chance.string({ pool: 'abcdefghijklmnopqrstuvwxyz' });
+    const stylesheet = css.parse(code);
+    const expectedSelector = `#widgets-container > ul > li[data-widget-id="${namespace}"] div`;
+    const rules = stylesheet.stylesheet.rules[0];
+
+    const selector = rules.rules[0].selectors[0];
+    expect(selector).toMatch('div');
+
+    namespaceRulesAST(stylesheet.stylesheet, namespace);
+
+    const namespacedSelector = rules.rules[0].selectors[0];
+    expect(namespacedSelector).toMatch(expectedSelector);
   });
 });

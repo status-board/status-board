1429cf408efe572aa2270410a87248c103405e89
James W. Lane <james.w.lane@mac.com>
2018-05-09 13:39:59 -0500

    test(config-manager): Improved testing of read-config-if-exists.ts

--- test/unit/config-manager/read-config-if-exists.test.ts
+++ test/unit/config-manager/read-config-if-exists.test.ts
@@ -1,7 +1,44 @@
+import * as fs from 'fs';
+
 import { readConfigIfExists } from '../../../src/config-manager/read-config-if-exists';
 
+jest.mock(
+  '../../../config/dummy.js',
+  () => {
+    return {
+      value: 'foo',
+    };
+  },
+  {
+    virtual: true,
+  },
+);
+
 describe('Config Manager: Read Config If Exists', () => {
-  test('Should match snapshot', () => {
-    expect(readConfigIfExists).toMatchSnapshot();
+  beforeEach(() => {
+    jest.spyOn(fs, 'existsSync')
+      .mockImplementation((fileName: string) => fileName.includes('dummy'));
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  test('Should return a empty object if no file exist', () => {
+    const configFile = readConfigIfExists('../../config/fail');
+
+    expect(configFile).toEqual({});
+  });
+
+  test('Should add a .js to file path', () => {
+    readConfigIfExists('../../config/pass');
+
+    expect(fs.existsSync).toHaveBeenCalledWith('../../config/pass.js');
+  });
+
+  test('Should return dummy file', () => {
+    const configFile = readConfigIfExists('../../config/dummy');
+
+    expect(configFile).toHaveProperty('value', 'foo');
   });
 });

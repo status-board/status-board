acef19205c8fdee17970749a528bb8bbf39dfff7
James W. Lane <james.w.lane@mac.com>
2018-05-25 13:21:13 -0500

    style(TSLint): Fixed lint errors

--- src/stylus/index.ts
+++ src/stylus/index.ts
@@ -1,4 +1,4 @@
-import { NextFunction,Request,Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import * as nib from 'nib';
 import * as realStylus from 'stylus';
 import { getStylusObject } from './get-stylus-object';

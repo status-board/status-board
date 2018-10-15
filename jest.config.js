9044e0c49ccba4c487d46a0743dc93fa589bf48c
James W. Lane <james.w.lane@mac.com>
2018-10-14 21:42:26 -0500

    test(ts-jest): Fixed Jest test runner

--- jest.config.js
+++ jest.config.js
@@ -2,29 +2,20 @@ module.exports = {
   bail: true,
   clearMocks: true,
   collectCoverageFrom: [
-    "src/**/*.{ts,tsx}",
+    'src/**/*.{ts,tsx}',
   ],
   coveragePathIgnorePatterns: [
-    "<rootDir>/test/helpers/",
-    "<rootDir>/node_modules/",
+    '<rootDir>/test/helpers/',
+    '<rootDir>/node_modules/',
   ],
   globals: {
-    "ts-jest": {
-      "tsConfigFile": "tsconfig.json",
-    }
+    'ts-jest': {
+      isolatedModules: true,
+      tsConfig: 'tsconfig.json',
+    },
   },
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node",
-  ],
   testMatch: [
-    "<rootDir>/test/**/*.(test|spec).(ts|tsx|js)",
+    '<rootDir>/test/**/*.(test|spec).(ts|tsx|js)',
   ],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
-};
+  preset: 'ts-jest',
+}
